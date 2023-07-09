73ef6f141cb73d90fd4400be5c95e30d25dd200c
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-07-09 22:01:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ class TrainingRecord {
 
         // Add Entry+ ボタンをクリック時、下記コールバック関数を実行。
         this.root.querySelector(".record__add").addEventListener("click", () => {
-            const date = new Date();
             // 現在の年を4桁の数字として取得しyear変数へ。
             const year = date.getFullYear();
             // getmonthメソッドに1をインクリメントして、1ベースの値に変換。
